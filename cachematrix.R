61a4dfb2aa093ff7ef372b702bc954c9edb0c285
moigithub <mcarlosman@gmail.com>
2014-07-16 09:43:03 -0500

    added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+# makeCacheMatrix function will store the matrix (original) data and the inverse matrix
+# and returns those data when requested
 makeCacheMatrix <- function(x = matrix()) {
         s <- NULL
         set <- function(y) {
@@ -17,11 +14,11 @@ makeCacheMatrix <- function(x = matrix()) {
              getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# cacheSolve function work togheter with makeCacheMatrix, 
+# this will check first
+# if inverse matrix was already solved and return
+# else solve and store it on "cache" for posterior use
+cacheSolve<- function(x, ...) {
         s <- x$getsolve()
         if(!is.null(s)) {
                 message("getting cached data")
@@ -30,5 +27,5 @@ cacheSolve <- function(x, ...) {
         data <- x$get()
         s <- solve(data, ...)
         x$setsolve(s)
-        s        
+        s
 }
